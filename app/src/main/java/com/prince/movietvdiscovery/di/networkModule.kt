2f468640c634489943c6b69ae9a25e8011bd7b2b
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 03:42:58 +0530

    feat: add Retrofit module with Koin

--- app/src/main/java/com/prince/movietvdiscovery/di/networkModule.kt
+++ app/src/main/java/com/prince/movietvdiscovery/di/networkModule.kt
@@ -4,6 +4,9 @@ import com.prince.movietvdiscovery.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = "https://api.watchmode.com/v1/"
 
@@ -22,6 +25,15 @@ val networkModule = module {
     single {
         OkHttpClient.Builder()
             .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 
