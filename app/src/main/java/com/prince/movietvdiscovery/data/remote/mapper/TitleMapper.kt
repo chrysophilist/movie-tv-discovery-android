f4a99947b0af3aa9abe3a0c31f00c2a1f968add1
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 05:08:12 +0530

    feat: add TitleMapper for movie and tv show domain conversion

---
+++ app/src/main/java/com/prince/movietvdiscovery/data/remote/mapper/TitleMapper.kt
@@ -0,0 +1,24 @@
+package com.prince.movietvdiscovery.data.remote.mapper
+
+import com.prince.movietvdiscovery.data.remote.dto.TitleDto
+import com.prince.movietvdiscovery.domain.model.Movie
+import com.prince.movietvdiscovery.domain.model.TvShow
+
+object TitleMapper {
+
+    fun toMovie(dto: TitleDto): Movie =
+        Movie(
+            id = dto.id,
+            title = dto.title,
+            year = dto.year,
+            type = dto.type,
+        )
+
+    fun toTvShow(dto: TitleDto): TvShow =
+        TvShow(
+            id = dto.id,
+            title = dto.title,
+            year = dto.year,
+            type = dto.type,
+        )
+}
