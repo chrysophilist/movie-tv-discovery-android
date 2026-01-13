d1f0be1eb0efe165f8f1e4337345c4037679ccb6
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 05:55:39 +0530

    feat: bind movie repository implementation in koin

--- app/src/main/java/com/prince/movietvdiscovery/di/RepositoryModule.kt
+++ app/src/main/java/com/prince/movietvdiscovery/di/RepositoryModule.kt
@@ -1,7 +1,12 @@
 package com.prince.movietvdiscovery.di
 
+import com.prince.movietvdiscovery.data.repository.RepositoryImpl
+import com.prince.movietvdiscovery.domain.repository.Repository
 import org.koin.dsl.module
 
 val repositoryModule = module {
 
+    single<Repository> {
+        RepositoryImpl(get())
+    }
 }
