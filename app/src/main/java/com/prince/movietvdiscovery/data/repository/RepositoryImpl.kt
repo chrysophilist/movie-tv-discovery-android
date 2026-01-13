3737df7907da97706010b8339de5ff3004aca708
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 05:49:16 +0530

    feat: implement repository using Single.zip for parallel movies and tv shows fetch

---
+++ app/src/main/java/com/prince/movietvdiscovery/data/repository/RepositoryImpl.kt
@@ -0,0 +1,43 @@
+package com.prince.movietvdiscovery.data.repository
+
+import com.prince.movietvdiscovery.data.remote.api.WatchmodeApi
+import com.prince.movietvdiscovery.data.remote.mapper.TitleMapper
+import com.prince.movietvdiscovery.domain.model.HomeContent
+import com.prince.movietvdiscovery.domain.repository.Repository
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.kotlin.Singles
+
+class RepositoryImpl (
+    private val api: WatchmodeApi
+): Repository {
+
+    override fun fetchHomeContent(): Single<HomeContent> {
+
+        val moviesSingle = api.getMovies()
+            .map { response ->
+                response.titles.map { dto ->
+                    TitleMapper.toMovie(dto)
+                }
+            }
+
+        val tvShowsSingle = api.getTvShows()
+            .map { response ->
+                response.titles.map { dto ->
+                    TitleMapper.toTvShow(dto)
+                }
+            }
+
+
+        return Singles.zip(
+            moviesSingle,
+            tvShowsSingle
+        ){ movies, tvShows ->
+            HomeContent(
+                movies = movies,
+                tvShows = tvShows
+            )
+        }
+
+    }
+
+}
