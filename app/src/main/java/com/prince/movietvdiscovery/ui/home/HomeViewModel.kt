fda0471c6d3e48236c38a2be2b2f527fa5b35046
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 06:38:10 +0530

    feat: implement HomeViewModel with Rx and StateFlow

---
+++ app/src/main/java/com/prince/movietvdiscovery/ui/home/HomeViewModel.kt
@@ -0,0 +1,49 @@
+package com.prince.movietvdiscovery.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.prince.movietvdiscovery.domain.model.HomeContent
+import com.prince.movietvdiscovery.domain.repository.Repository
+import com.prince.movietvdiscovery.ui.common.UiState
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeViewModel(
+    private val repo: Repository
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<HomeContent>>(UiState.Loading)
+    val uiState: StateFlow<UiState<HomeContent>> = _uiState
+
+    private val disposables = CompositeDisposable()
+
+    init {
+        fetchHomeContent()
+    }
+
+    private fun fetchHomeContent(){
+
+        _uiState.value = UiState.Loading
+
+        repo.fetchHomeContent()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { data ->
+                    _uiState.value = UiState.Success(data)
+                },
+                onError = { error ->
+                    _uiState.value = UiState.Error(error.message ?: "Unknown Error")
+                }
+            )
+            .addTo(disposables)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
+    }
+}
