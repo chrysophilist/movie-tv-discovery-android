05db1b74cddc797ddd76a07658a8c98e763950a7
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 18:07:02 +0530

    feat: add Watchmode API endpoint for title details

--- app/src/main/java/com/prince/movietvdiscovery/data/remote/api/WatchmodeApi.kt
+++ app/src/main/java/com/prince/movietvdiscovery/data/remote/api/WatchmodeApi.kt
@@ -2,6 +2,7 @@ package com.prince.movietvdiscovery.data.remote.api
 
 import com.prince.movietvdiscovery.BuildConfig
 import com.prince.movietvdiscovery.data.remote.dto.ListTitlesResponseDto
+import com.prince.movietvdiscovery.data.remote.dto.TitleDetailsDto
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -23,4 +24,11 @@ interface WatchmodeApi {
         @Query("page") page: Int = 1,
         @Query("limit") limit: Int = 20
     ): Single<ListTitlesResponseDto>
+
+
+    @GET("title/{title_id}/details/")
+    fun getTitleDetails(
+        @Query("apiKey") apiKey: String = BuildConfig.WATCHMODE_API_KEY,
+        @Query("titleId") titleId: Int
+    ): Single<TitleDetailsDto>
 }
