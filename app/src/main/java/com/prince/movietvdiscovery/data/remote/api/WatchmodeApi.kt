f6adc229a63acc1b643cf26449da6f90cace4d6b
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 05:30:31 +0530

    feat: add Watchmode API endpoints for movies and tv shows

---
+++ app/src/main/java/com/prince/movietvdiscovery/data/remote/api/WatchmodeApi.kt
@@ -0,0 +1,26 @@
+package com.prince.movietvdiscovery.data.remote.api
+
+import com.prince.movietvdiscovery.BuildConfig
+import com.prince.movietvdiscovery.data.remote.dto.ListTitlesResponseDto
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WatchmodeApi {
+
+    @GET("list-titles/")
+    fun getMovies(
+        @Query("apiKey") apiKey: String = BuildConfig.WATCHMODE_API_KEY,
+        @Query("types") types: String = "movie",
+        @Query("page") page: Int = 1,
+        @Query("limit") limit: Int = 20
+    ): Single<ListTitlesResponseDto>
+
+    @GET("list-titles/")
+    fun getTvShows(
+        @Query("apiKey") apiKey: String = BuildConfig.WATCHMODE_API_KEY,
+        @Query("types") types: String = "tv_series",
+        @Query("page") page: Int = 1,
+        @Query("limit") limit: Int = 20
+    ): Single<ListTitlesResponseDto>
+}
