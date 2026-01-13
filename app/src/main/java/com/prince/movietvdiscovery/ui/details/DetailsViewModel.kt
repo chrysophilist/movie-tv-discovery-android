d5f5d17b58ec3b5135d381dea79740e732825c2c
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 18:39:40 +0530

    feat: implement DetailsViewModel with Rx and StateFlow

---
+++ app/src/main/java/com/prince/movietvdiscovery/ui/details/DetailsViewModel.kt
@@ -0,0 +1,49 @@
+package com.prince.movietvdiscovery.ui.details
+
+import androidx.lifecycle.ViewModel
+import com.prince.movietvdiscovery.domain.repository.Repository
+import com.prince.movietvdiscovery.ui.common.DetailsUiState
+import com.prince.movietvdiscovery.ui.common.UiState
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.addTo
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class DetailsViewModel (
+    private val repo: Repository
+): ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<DetailsUiState>>(UiState.Loading)
+    val uiState: StateFlow<UiState<DetailsUiState>> = _uiState
+
+    private val disposables = CompositeDisposable()
+
+    fun loadDetails(titleId: Int) {
+
+        _uiState.value = UiState.Loading
+
+        repo.fetchTitleDetails(titleId)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onSuccess = { details ->
+                    _uiState.value = UiState.Success(
+                        DetailsUiState(details)
+                    )
+                },
+                onError = { error ->
+                    _uiState.value = UiState.Error(
+                        error.message ?: "Unknown Error"
+                    )
+                }
+            )
+            .addTo(disposables)
+    }
+
+    override fun onCleared() {
+        disposables.clear()
+    }
+}
