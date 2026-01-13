96e7ae7933b5e17ba1d4b9f023eef16892849d0e
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 06:39:45 +0530

    feat: register HomeViewModel in koin

--- app/src/main/java/com/prince/movietvdiscovery/di/ViewModelModule.kt
+++ app/src/main/java/com/prince/movietvdiscovery/di/ViewModelModule.kt
@@ -1,7 +1,14 @@
 package com.prince.movietvdiscovery.di
 
+import com.prince.movietvdiscovery.ui.home.HomeViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
+    viewModel {
+        HomeViewModel(
+            repo = get()
+        )
+    }
 }
