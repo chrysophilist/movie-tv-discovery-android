a25cb3df7ab4457944d49db6078dc44c80f5b93a
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-13 18:02:32 +0530

    feat: define repository interface for title details

--- app/src/main/java/com/prince/movietvdiscovery/domain/repository/Repository.kt
+++ app/src/main/java/com/prince/movietvdiscovery/domain/repository/Repository.kt
@@ -1,9 +1,12 @@
 package com.prince.movietvdiscovery.domain.repository
 
 import com.prince.movietvdiscovery.domain.model.HomeContent
+import com.prince.movietvdiscovery.domain.model.TitleDetails
 import io.reactivex.rxjava3.core.Single
 
 interface Repository {
 
     fun fetchHomeContent(): Single<HomeContent>
+
+    fun fetchTitleDetails(titleId: Int): Single<TitleDetails>
 }
