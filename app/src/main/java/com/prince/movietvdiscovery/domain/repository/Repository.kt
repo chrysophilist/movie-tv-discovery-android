2901f61cfa1e3d12f53f806ea5514b9e9645e7c0
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-17 10:10:30 +0530

    refactor(domain): migrate repository interface to suspend functions

--- app/src/main/java/com/prince/movietvdiscovery/domain/repository/Repository.kt
+++ app/src/main/java/com/prince/movietvdiscovery/domain/repository/Repository.kt
@@ -1,13 +1,16 @@
 package com.prince.movietvdiscovery.domain.repository
 
 import com.prince.movietvdiscovery.domain.model.HomeContent
+import com.prince.movietvdiscovery.domain.model.TitleDetails
 import com.prince.movietvdiscovery.domain.model.TitleDetailsWithSources
 import com.prince.movietvdiscovery.domain.util.Result
 import io.reactivex.rxjava3.core.Single
 
 interface Repository {
 
-    fun fetchHomeContent(): Single<Result<HomeContent>>
+    suspend fun fetchHomeContent(): Result<HomeContent>
 
-    fun fetchTitleDetailsWithSources(titleId: Int): Single<Result<TitleDetailsWithSources>>
+    suspend fun fetchTitleDetails(titleId: Int): Result<TitleDetails>
+
+    suspend fun fetchTitleDetailsWithSources(titleId: Int): Result<TitleDetailsWithSources>
 }
