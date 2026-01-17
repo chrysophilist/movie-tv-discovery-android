7662ae4e6e6aad065432e6f694eb4aea53de0438
chrysophilist <100132795+chrysophilist@users.noreply.github.com>
2026-01-17 09:57:07 +0530

    chore(di): provide DispatcherProvider via Koin

--- app/src/main/java/com/prince/movietvdiscovery/di/RepositoryModule.kt
+++ app/src/main/java/com/prince/movietvdiscovery/di/RepositoryModule.kt
@@ -2,11 +2,17 @@ package com.prince.movietvdiscovery.di
 
 import com.prince.movietvdiscovery.data.repository.RepositoryImpl
 import com.prince.movietvdiscovery.domain.repository.Repository
+import com.prince.movietvdiscovery.domain.util.DefaultDispatcherProvider
+import com.prince.movietvdiscovery.domain.util.DispatcherProvider
 import org.koin.dsl.module
 
 val repositoryModule = module {
 
     single<Repository> {
         RepositoryImpl(get())
+    }
+
+    single<DispatcherProvider> {
+        DefaultDispatcherProvider()
     }
 }
